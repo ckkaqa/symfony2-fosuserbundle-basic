886e7ec398e4a03462b69123f40c749598fb2f8c
jhunnie <jhunnie.ramos20@gmail.com>
2019-01-16 22:43:21 +0800

    hwioauth facebook

--- src/AppBundle/Entity/FOSUBUserProvider.php
+++ src/AppBundle/Entity/FOSUBUserProvider.php
@@ -1,95 +1,187 @@
 <?php
 
-// Change the namespace according to your project.
+/*
+ * This file is part of the HWIOAuthBundle package.
+ *
+ * (c) Hardware.Info <opensource@hardware.info>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace AppBundle\Entity;
 
+use FOS\UserBundle\Model\User;
+use FOS\UserBundle\Model\UserManagerInterface;
+use HWI\Bundle\OAuthBundle\Connect\AccountConnectorInterface;
 use HWI\Bundle\OAuthBundle\OAuth\Response\UserResponseInterface;
-use HWI\Bundle\OAuthBundle\Security\Core\User\FOSUBUserProvider as BaseClass;
+use HWI\Bundle\OAuthBundle\Security\Core\Exception\AccountNotLinkedException;
+use Symfony\Component\PropertyAccess\PropertyAccess;
+use Symfony\Component\PropertyAccess\PropertyAccessor;
+use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
+use Symfony\Component\Security\Core\Exception\UsernameNotFoundException;
 use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\User\UserProviderInterface;
+use HWI\Bundle\OAuthBundle\Security\Core\User\OAuthAwareUserProviderInterface;
 
+/**
+ * Class providing a bridge to use the FOSUB user provider with HWIOAuth.
+ *
+ * In order to use the class as a connector, the appropriate setters for the
+ * property mapping should be available.
+ *
+ * @author Alexander <iam.asm89@gmail.com>
+ */
+class FOSUBUserProvider implements UserProviderInterface, AccountConnectorInterface, OAuthAwareUserProviderInterface
+{
+    /**
+     * @var UserManagerInterface
+     */
+    protected $userManager;
 
-// Source: https://gist.github.com/danvbe/4476697
+    /**
+     * @var array
+     */
+    protected $properties = array(
+        'identifier' => 'id',
+    );
 
-class FOSUBUserProvider extends BaseClass {
+    /**
+     * @var PropertyAccessor
+     */
+    protected $accessor;
 
-    public function connect(UserInterface $user, UserResponseInterface $response) {
-        $property = $this->getProperty($response);
-        
-        $username = $response->getUsername();
-        
-        // On connect, retrieve the access token and the user id
-        $service = $response->getResourceOwner()->getName();
-        
-        $setter = 'set' . ucfirst($service);
-        $setter_id = $setter . 'Id';
-        $setter_token = $setter . 'AccessToken';
-        
-        // Disconnect previously connected users
-        if (null !== $previousUser = $this->userManager->findUserBy(array($property => $username))) {
-            $previousUser->$setter_id(null);
-            $previousUser->$setter_token(null);
-            $this->userManager->updateUser($previousUser);
+    /**
+     * Constructor.
+     *
+     * @param UserManagerInterface $userManager fOSUB user provider
+     * @param array                $properties  property mapping
+     */
+    public function __construct(UserManagerInterface $userManager, array $properties)
+    {
+        $this->userManager = $userManager;
+        $this->properties = array_merge($this->properties, $properties);
+        $this->accessor = PropertyAccess::createPropertyAccessor();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function loadUserByUsername($username)
+    {
+        // Compatibility with FOSUserBundle < 2.0
+        if (class_exists('FOS\UserBundle\Form\Handler\RegistrationFormHandler')) {
+            return $this->userManager->loadUserByUsername($username);
         }
-        
-        // Connect using the current user
-        $user->$setter_id($username);
-        $user->$setter_token($response->getAccessToken());
+
+        return $this->userManager->findUserByUsername($username);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function loadUserByOAuthUserResponse(UserResponseInterface $response)
+    {
+        $username = $response->getUsername();
+
+        $user = $this->userManager->findUserBy(array($this->getProperty($response) => $username));
+        // die('22');
+        if (null === $user || null === $username) {
+            throw new AccountNotLinkedException(sprintf("User '%s' not found.", $username));
+        }
+
+        return $user;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function connect(UserInterface $user, UserResponseInterface $response)
+    {
+        if (!$user instanceof User) {
+            throw new UnsupportedUserException(sprintf('Expected an instance of FOS\UserBundle\Model\User, but got "%s".', get_class($user)));
+        }
+
+        $property = $this->getProperty($response);
+        $username = $response->getUsername();
+
+        if (null !== $previousUser = $this->userManager->findUserBy(array($property => $username))) {
+            $this->disconnect($previousUser, $response);
+        }
+
+        if ($this->accessor->isWritable($user, $property)) {
+            $this->accessor->setValue($user, $property, $username);
+        } else {
+            throw new \RuntimeException(sprintf('Could not determine access type for property "%s".', $property));
+        }
+
         $this->userManager->updateUser($user);
     }
 
-    public function loadUserByOAuthUserResponse(UserResponseInterface $response) {
-        $data = $response->getResponse();
-        $username = $response->getUsername();
-        $email = $response->getEmail() ? $response->getEmail() : $username;
-        $user = $this->userManager->findUserBy(array($this->getProperty($response) => $username));
-        
-        // If the user is new
-        if (null === $user) {
-            $service = $response->getResourceOwner()->getName();
-            $setter = 'set' . ucfirst($service);
-            $setter_id = $setter . 'Id';
-            $setter_token = $setter . 'AccessToken';
-            // create new user here
-            $user = $this->userManager->createUser();
-            $user->$setter_id($username);
-            $user->$setter_token($response->getAccessToken());
-            
-            //I have set all requested data with the user's username
-            //modify here with relevant data
-            $user->setUsername($this->generateRandomUsername($username, $response->getResourceOwner()->getName()));
-            $user->setEmail($email);
-            $user->setPassword($username);
-            $user->setEnabled(true);
-            $this->userManager->updateUser($user);
-            return $user;
+    /**
+     * Disconnects a user.
+     *
+     * @param UserInterface         $user
+     * @param UserResponseInterface $response
+     */
+    public function disconnect(UserInterface $user, UserResponseInterface $response)
+    {
+        $property = $this->getProperty($response);
+
+        $this->accessor->setValue($user, $property, null);
+        $this->userManager->updateUser($user);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function refreshUser(UserInterface $user)
+    {
+        // Compatibility with FOSUserBundle < 2.0
+        if (class_exists('FOS\UserBundle\Form\Handler\RegistrationFormHandler')) {
+            return $this->userManager->refreshUser($user);
         }
-        
-        // If the user exists, use the HWIOAuth
-        $user = parent::loadUserByOAuthUserResponse($response);
-        
-        $serviceName = $response->getResourceOwner()->getName();
-        
-        $setter = 'set' . ucfirst($serviceName) . 'AccessToken';
-        
-        // Update the access token
-        $user->$setter($response->getAccessToken());
-        
+
+        $identifier = $this->properties['identifier'];
+        if (!$user instanceof User || !$this->accessor->isReadable($user, $identifier)) {
+            throw new UnsupportedUserException(sprintf('Expected an instance of FOS\UserBundle\Model\User, but got "%s".', get_class($user)));
+        }
+
+        $userId = $this->accessor->getValue($user, $identifier);
+        if (null === $user = $this->userManager->findUserBy(array($identifier => $userId))) {
+            throw new UsernameNotFoundException(sprintf('User with ID "%d" could not be reloaded.', $userId));
+        }
+
         return $user;
     }
-    
+
     /**
-     * Generates a random username with the given 
-     * e.g 12345_github, 12345_facebook
-     * 
-     * @param string $username
-     * @param type $serviceName
-     * @return type
+     * {@inheritdoc}
      */
-    private function generateRandomUsername($username, $serviceName){
-        if(!$username){
-            $username = "user". uniqid((rand()), true) . $serviceName;
+    public function supportsClass($class)
+    {
+        $userClass = $this->userManager->getClass();
+
+        return $userClass === $class || is_subclass_of($class, $userClass);
+    }
+
+    /**
+     * Gets the property for the response.
+     *
+     * @param UserResponseInterface $response
+     *
+     * @return string
+     *
+     * @throws \RuntimeException
+     */
+    protected function getProperty(UserResponseInterface $response)
+    {
+        $resourceOwnerName = $response->getResourceOwner()->getName();
+
+        if (!isset($this->properties[$resourceOwnerName])) {
+            throw new \RuntimeException(sprintf("No property defined for entity for resource owner '%s'.", $resourceOwnerName));
         }
-        
-        return $username. "_" . $serviceName;
+
+        return $this->properties[$resourceOwnerName];
     }
 }
